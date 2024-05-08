f31da11b9480e4a87d77390f055cce3d1cdb5e98
ABE <abeabeabeabe444@gmail.com>
2024-05-08 14:46:44 +0700

    ✨ IMDB code search

--- app/Filament/Resources/AllSubResource.php
+++ app/Filament/Resources/AllSubResource.php
@@ -55,7 +55,7 @@ class AllSubResource extends Resource
                 Tables\Columns\TextColumn::make('title')
                     ->searchable(),
                 Tables\Columns\TextColumn::make('imdb')
-                    ->numeric()
+                    ->searchable()
                     ->sortable(),
                 Tables\Columns\TextColumn::make('date')
                     ->dateTime()
